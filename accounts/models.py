119d7cebc1b660cdac5c879f780e167007a97e99
Proton <proton@MacBook-Pro-Proton.local>
2026-06-18 11:09:07 +0500

    changes by proton

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from rest_framework.views import APIView
+from rest_framework_simplejwt import models
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 class User(AbstractUser):
-    pass
+    @property
+    def token(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+    class Meta:
+        db_table = 'user'
---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenBlacklistView,
+)
+
+urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/blacklist/', TokenBlacklistView.as_view()),
+]
