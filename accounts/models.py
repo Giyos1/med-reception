57c08daf7a7c559051be5ee4f6c557d839130d51
G'iyos Master <86424045+Giyos1@users.noreply.github.com>
2026-06-20 19:16:01 +0500

    Revert "changes by proton"

--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,4 @@
 from django.contrib.auth.models import AbstractUser
-from rest_framework_simplejwt import models
-from django.db import models
-
 
 class User(AbstractUser):
     pass
-
-
-class PatientApplication(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    condition = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        db_table = 'patient_application'
