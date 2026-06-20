19d68ce6422993adf9f2fd9222a4c9b9d26e392b
giyos <giyos@giyoss-MacBook-Pro.local>
2026-06-20 19:34:59 +0500

    fix

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,7 @@
 from django.contrib import admin
-from django.urls import path,include
-from users.views import LoginView, LogoutView, CustomTokenRefreshView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('users.urls')),
-    path('api/auth/login/', LoginView.as_view(), name='auth_login'),
-    path('api/auth/logout/', LogoutView.as_view(), name='auth_logout'),
-    path('api/auth/token/refresh/', CustomTokenRefreshView.as_view(), name='auth_token_refresh'),
 ]
