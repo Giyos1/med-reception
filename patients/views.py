c168c8bea50560bf216cb16a5b90be38e5554d03
Proton <proton@MacBook-Pro-Proton.local>
2026-06-25 00:01:07 +0500

    submit

--- patients/views.py
+++ patients/views.py
@@ -1,1 +1,11 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+from patients.models import Patient
+from patients.serialazer import PatientSubmitSerializer
+
+
+class PatientView(APIView):
+    serializer_class=PatientSubmitSerializer
+    queryset=Patient.objects.all()
+    permission_classes=(AllowAny,)
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from rest_framework import routers
+from patients.views import PatientView
 from users.views import LoginView, LogoutView, CustomTokenRefreshView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='auth_login'),
     path('logout/', LogoutView.as_view(), name='auth_logout'),
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='auth_token_refresh'),
+    path ('patients/', PatientView.as_view(), name='patients'),
 ]
